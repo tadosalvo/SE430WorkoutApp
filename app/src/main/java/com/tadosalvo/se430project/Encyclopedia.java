3ce88d01e6d0b4f2fe16febf82fd2b8e7ae85f08
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2023-05-26 22:26:31 -0700

    Add files via upload

---
+++ app/src/main/java/com/tadosalvo/se430project/Encyclopedia.java
@@ -0,0 +1,12 @@
+package com.tadosalvo.se430project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Encyclopedia {
+    
+}
