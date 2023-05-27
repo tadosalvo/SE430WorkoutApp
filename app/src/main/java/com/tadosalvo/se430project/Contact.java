95f56246f9e4d6fada02aafdfefb11c161e2eb1f
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2023-05-26 22:21:10 -0700

    Add files via upload

---
+++ app/src/main/java/com/tadosalvo/se430project/Contact.java
@@ -0,0 +1,20 @@
+package com.tadosalvo.se430project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Contact {
+
+
+    public void Categories {
+
+    }
+
+    public void Terms {
+
+    }
+}
